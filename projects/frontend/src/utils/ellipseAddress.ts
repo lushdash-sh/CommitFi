ed7f974ad9d935e25a0ca71e08c18429f71ccc65
Shruti Bhosale <shrutib1250@gmail.com>
2026-04-16 20:15:22 +0530

    fixed business and fitness section

--- projects/frontend/src/utils/ellipseAddress.ts
+++
@@ -1,6 +0,0 @@
-export function ellipseAddress(address: string = '', width: number = 6): string {
-  if (!address) {
-    return ''
-  }
-  return `${address.slice(0, width)}...${address.slice(-width)}`
-}
